1819daff9eec2c72b556ce5ad761e0ecfabc17f0
M-A-SAIADITHYAA <145114909+M-A-SAIADITHYAA@users.noreply.github.com>
2025-05-22 15:59:50 +0530

    feat: add Header and Hero components with navigation and typewriter effect
    - Implemented Header component with navigation links and a RainbowButton.
    - Created Hero component featuring a typewriter effect and buttons for user interaction.
    - Added utility functions for class name management.
    - Introduced ShootingStars and StarsBackground components for enhanced visual effects.
    - Developed reusable UI components including Button and HeroHighlight.
    - Integrated CSS for styling and animations, including a custom rainbow animation.
    - Set up TypeScript configuration for improved type safety and development experience.
    - Configured Vite for React with Tailwind CSS support.

---
+++ src/components/Hero.tsx
@@ -0,0 +1,51 @@
+import React from 'react'
+import { HeroHighlight } from './ui/hero-highlight'
+import { Sparkle } from 'lucide-react'
+import { TypewriterEffectSmooth } from './ui/typewriter-effect'
+import { Button } from './ui/button'
+import { ShootingStars } from './ui/shooting-stars'
+import { StarsBackground } from './ui/stars-background'
+
+const Hero = () => {
+  const words = [{
+
+    text:"Only"
+        
+      },{
+        text:"Source"
+      },{
+        text:"Of"
+
+      },{text:"News"},{
+        text:"You"
+      },{
+        text:"Need"
+      }]
+  return (
+    <div id ="hero" >
+      <div className="flex flex-col items-center justify-center h-[40rem]">
+        <p className='text-xs sm:text-5xl text-neutral-600 '>
+          Legacy media is corrupted
+        </p>
+        <TypewriterEffectSmooth words={words}/>
+        <div className='flex flex-col md:flex-row  space-x-2 md:space-x-5'>
+           <Button className='w-40 h-10 rounded-xl bg-black border border-accent dark:border-white text-white text-md '>
+        Visit now
+      </Button>
+      <Button className='w-40 h-10 rounded-xl bg-black border border-accent dark:border-white text-white text-md '>
+        Learn More
+      </Button>
+      <ShootingStars />
+      <StarsBackground />
+        </div>
+      </div>
+     
+      
+    </div>
+   
+
+
+  )
+}
+
+export default Hero
---
+++ src/components/nav.ts
@@ -0,0 +1,9 @@
+export const navData = [
+    {title:"Features",href:"#Features"},
+    {title:"Sample",href:"#Sample"},
+    {title:"Testimonials",href:"#Testimonials"},
+    {title:"Pricing",href:"#Pricing"},
+    
+    
+
+]
